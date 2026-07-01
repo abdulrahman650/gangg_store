3649a091b86a89253a731678c4e18725e38e1a0a
abdulrahman650 <114238017+abdulrahman650@users.noreply.github.com>
2026-07-01 23:50:59 +0300

    add radius elvated button

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -139,7 +139,7 @@ class AppTheme {
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
-        // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       ),
     ),
 
